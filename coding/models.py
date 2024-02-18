f097493720c672f3d8e04271c09780cf28cce0dd
soru4 <73024932+soru4@users.noreply.github.com>
2024-02-17 21:15:46 -0600

    hi

--- coding/models.py
+++ coding/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Code(models.Model):
+    text = models.TextField()
+
+class PracticeTest(models.Model):
+    testName = models.TextField()
+    qu
+
+class Questions(models.Model):
+    
