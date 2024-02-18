3b68be3ad61c0fcb1e0e42f9a19f8f2937fbeda2
soru4 <73024932+soru4@users.noreply.github.com>
2024-02-17 22:08:50 -0600

    yes

--- coding/models.py
+++ coding/models.py
@@ -1,12 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Code(models.Model):
     text = models.TextField()
+    user = models.ForeignKey(User, verbose_name=(""), on_delete=models.CASCADE)
+
+class Choices(models.Model):
+    choiceText = models.TextField((""))
+    correct = models.TextField((""))
+
+class Questions(models.Model):
+    questionTitle = models.TextField()
+    questionChoices = models.ManyToManyField(Choices, verbose_name=(""))
 
 class PracticeTest(models.Model):
     testName = models.TextField()
-    qu
-
-class Questions(models.Model):
-    
+    questions = models.ManyToManyField(Questions, verbose_name=(""))
--- coding/views.py
+++ coding/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def loadHomeScreen(request):
+    return render(request, "home.html");
+
+
+ 
